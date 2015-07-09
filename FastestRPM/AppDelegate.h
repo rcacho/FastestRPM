8a240071b3ba2ea60662936b955f8842035f35d0
rcacho <rcacho@hotmail.ca>
2015-07-09 10:31:27 -0700

    init

---
+++ FastestRPM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastestRPM
+//
+//  Created by ricardo antonio cacho on 2015-07-09.
+//  Copyright (c) 2015 ricardo antonio cacho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
